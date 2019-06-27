5120e9ffefff4a221e856195e87fab560200997a
cbroccoli <39035570+cbroccoli@users.noreply.github.com>
2019-06-27 19:00:28 -0400

    Update install_ansible.py

--- scripts/install_ansible.py
+++ scripts/install_ansible.py
@@ -8,6 +8,12 @@ import os
 import time
 import re
 
+# Update the variables with the values for the current project and zone before executing the 
+# depolyment.
+
+PROJECT           = "PROJECT"
+ZONE              = "ZONE"
+
 def update_host():
     
 subprocess.call(['apt-get', 'update', '-y')
@@ -46,6 +52,19 @@ f_new.close()
 
 subprocess.call(['sudo', 'mv', '/etc/ansible/ansible.cfg', 'ansible-config.bak'])
 subprocess.call(['sudo', 'mv', '/etc/ansible/ansible-config-new.cfg', '/etc/ansible/ansible.cfg'])
+
+# Add the working directories
+
+subprocess.call(['mkdir', '/etc/ansible/playbooks'])
+subprocess.call(['mkdir', '/etc/ansible/inventory'])
+
+# Add the basic framework for the configuration file which will allow Ansible to access
+# the GCP inventory for the project. Update the ZONE AND PROJECT variables (including the 
+# []) with the correct values.
+
+f_inventory = open("/etc/ansible/inventory/inventoy.gcp.yml", "w")
+f_inventory.write("plugin: gcp_compute \nzones: \n{} \nprojects: \n - google.com:{} \nfilters: null \nauth_kind: serviceaccount \nservice_account_file: INSERT_JSON_FILEPATH".format(ZONE, PROJECT))
+f_inventory.close()
 
 # END configure_ansible()
 
